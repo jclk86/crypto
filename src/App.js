aaec2764e7eaf1c843ced29711bd17556f5fffd5
John Chan <johnchancontact@gmail.com>
2023-07-14 18:16:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Heading, Image, Text, Button } from '@chakra-ui/react';
+import NavBar from './components/navigation/NavBar';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box minHeight="100vh" display="flex" flexDirection="column">
+      <NavBar />
+      <Box
+        flexGrow="1"
+        display="flex"
+        flexDirection={{ base: 'column', md: 'row' }}
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Box flex="1" textAlign="center">
+          <Heading size="2xl">BTHS CRYPTO</Heading>
+          <Box>
+            <Text as="b" fontSize="2xl">
+              Welcome to the Crypto Club, where we unravel the mysteries of the
+              future of money while ensuring an enjoyable and exciting journey
+              for all. Our mission is to make the world of cryptocurrencies and
+              blockchain technology accessible, understandable, and entertaining
+              for everyone.
+            </Text>
+          </Box>
+          <Box>
+            <Button>Join</Button>
+          </Box>
+        </Box>
+        <Box
+          flex="1"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Image
+            src="/images/hero_crypto.jpg"
+            alt="Cryptocurrency"
+            maxW="100%"
+            h="auto"
+          />
+        </Box>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection={{ base: 'column', md: 'row' }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box flex="1" textAlign="center">
+          <Heading size="2xl">Why Learn Crypto?</Heading>
+          <Box>
+            <Text as="b" fontSize="2xl">
+              With the increasing adoption of blockchain technology, learning
+              crypto opens doors to exciting career opportunities in various
+              sectors such as finance, technology, entrepreneurship, and beyond,
+              positioning individuals at the forefront of innovation in the
+              digital era.
+            </Text>
+          </Box>
+          <Box>
+            <Button>Join the Fun</Button>
+          </Box>
+        </Box>
+        <Box
+          flex="1"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Image
+            src="/images/second_image.jpg"
+            alt="Second Image"
+            maxW="100%"
+            h="auto"
+          />
+        </Box>
+      </Box>
+      <Footer />
+    </Box>
   );
 }
+
+// function App() {
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('/users');
+//         setUsers(response.data.users); // Assuming the response data has a 'users' property
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       {users.length > 0 ? (
+//         users.map((user, i) => <div key={i}>{user.name}</div>)
+//       ) : (
+//         <div>Loading...</div>
+//       )}
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+
+// Errors regarding needing a loader was due to react scripts version being too old
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
